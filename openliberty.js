8c4dbe5d759ffe00e8005ea319594578795c8179
Austin <Austin.Bailey@ibm.com>
2020-12-17 15:40:35 +0000

    Switch to querySelector

--- openliberty.js
+++ openliberty.js
@@ -1,42 +1,44 @@
 function SSOtext() {
-    var GitHub_enabled = document.getElementsByClassName('login-oa2-github')[0]
-    var Google_enabled = document.getElementsByClassName('login-oa2-google-oauth2')[0]
-    var IBMid_enabled = document.getElementsByClassName('login-oa2-ibmid')[0]
-    var LinkedIn_enabled = document.getElementsByClassName('login-oa2-linkedin-oauth2')[0]
+    var GitHub_enabled = document.querySelector('.button-oa2-github')
+    var Google_enabled = document.querySelector('.button-oa2-google-oauth2')
+    var IBMid_enabled = document.querySelector('.button-oa2-ibmid')
+    var LinkedIn_enabled = document.querySelector('.button-oa2-linkedin-oauth2')
 
     if (GitHub_enabled) {
-    document.getElementsByClassName('login-oa2-github')[0].getElementsByTagName('span')[0].textContent = 'Sign in with GitHub'
+    GitHub_enabled.textContent = 'Sign in with GitHub'
     }
     if (Google_enabled) {
-    document.getElementsByClassName('login-oa2-google-oauth2')[0].getElementsByTagName('span')[0].textContent = 'Sign in with Google'
+    Google_enabled.textContent = 'Sign in with Google'
     }
     if (IBMid_enabled) {
-    document.getElementsByClassName('login-oa2-ibmid')[0].getElementsByTagName('span')[0].textContent = 'Sign in with IBMid'
+    IBMid_enabled.textContent = 'Sign in with IBMid'
     }
     if (LinkedIn_enabled) {
-    document.getElementsByClassName('login-oa2-linkedin-oauth2')[0].getElementsByTagName('span')[0].textContent = 'Sign in with LinkedIn'
+    LinkedIn_enabled.textContent = 'Sign in with LinkedIn'
     }
 }
 
 function addAccountReqText() {
-    var New_User_Text = document.getElementsByClassName('toggle-form')[0]
+    var newUserText = document.querySelector('.toggle-form')
 
-    if (New_User_Text) {
-        New_User_Text.insertAdjacentHTML('beforebegin', '<span class="text">To prevent abuse of our cloud hosted environments we require users to authenticate themselves, this can be done via the provided social login options or by creating a account.</span>');
+    if (newUserText) {
+        const span = document.createElement('span');
+        span.textContent = 'To prevent abuse of our cloud hosted environments we require users to authenticate themselves, this can be done via the provided social login options or by creating a account.';
+        newUserText.insertBefore(span);
     }
 }
 
 function fixRegisterPage() {
-    var SSOused = document.getElementsByClassName('js-auth-warning status')[0]
-    var register_form = document.getElementById('register')
-    var login_form = document.getElementById('login')
+    var ssoUsed = document.querySelector('.js-auth-warning .status')
+    var registerForm = document.getElementById('register')
+    var loginForm = document.getElementById('login')
 
-    if (SSOused) {
-        if (register_form) {
-            register_form.style.display = "block";
+    if (ssoUsed) {
+        if (registerForm) {
+            registerForm.style.display = "block";
         }
-        if (login_form) {
-            login_form.style.display = "block";
+        if (loginForm) {
+            loginForm.style.display = "block";
         }
     }
 }
